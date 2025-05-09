a7b3e795747ded01c9e7cb7042ed4b62044da681
Rishika B S <rishika1826@gmail.com>
2025-05-09 22:09:15 +0530

    Update utilityFunctions.py

--- utilityFunctions.py
+++ utilityFunctions.py
@@ -8,13 +8,11 @@ import psycopg2
 def db_connection(db_name):
     conn = None
     try:
-        # Deployed (Heroku postgres)
-        # if db_name == "users":
-        #     conn = psycopg2.connect("postgres://mhldgvpkokhorj:84b82ac4b9d5b6834e371a2031d243a0c8eb52d779c85bc140ca1ddbcefa5415@ec2-3-211-221-185.compute-1.amazonaws.com:5432/dak9afk7el8um7")
-        # else:
-            conn = sqlite3.connect("./model/data/"+db_name+".sqlite")
-    except sqlite3.error as e:
-        print(e)
+        # SQLite connection with timeout and WAL mode
+        conn = sqlite3.connect("./model/data/" + db_name + ".sqlite", timeout=10)  # Wait up to 10 seconds if locked
+        conn.execute("PRAGMA journal_mode=WAL;")  # Enable Write-Ahead Logging for better concurrency
+    except sqlite3.Error as e:
+        print(f"Database connection error: {e}")
     return conn
 
 
@@ -23,18 +21,27 @@ def fetchTrailer(movie_id, movies_result):
     for row in movies_result:
         if row[1] == movie_id:
             movie_index = row[0]
-            break;
+            break
     key = movies_result[movie_index][12]
     return key
 
 
 # Get Backdrop image path
 def fetchBackdrop(movie_id):
-    response = requests.get(
-        'https://api.themoviedb.org/3/movie/{}?api_key=65669b357e1045d543ba072f7f533bce&language=en-US'.format(
-            movie_id))
-    data = response.json()
-    return "https://image.tmdb.org/t/p/original"+str(data['backdrop_path'])
+    try:
+        response = requests.get(
+            f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=65669b357e1045d543ba072f7f533bce&language=en-US',
+            timeout=10  # Set a timeout of 10 seconds
+        )
+        response.raise_for_status()  # Raise an HTTPError for bad responses
+        data = response.json()
+        return "https://image.tmdb.org/t/p/original" + str(data.get('backdrop_path', ''))
+    except requests.exceptions.Timeout:
+        print(f"Request to fetch backdrop for movie_id {movie_id} timed out.")
+        return "https://via.placeholder.com/500x281?text=No+Image+Available"
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return "https://via.placeholder.com/500x281?text=No+Image+Available"
 
 
 # Get poster image path
@@ -42,14 +49,13 @@ def fetchPoster(movie_id, movies_result):
     for row in movies_result:
         if row[1] == movie_id:
             movie_index = row[0]
-            break;
+            break
     poster = movies_result[movie_index][11]
     return poster
 
 
 # Get 3 recommended movies based on genres and cast chosen by user
 def byChoice(genreList, castList):
-
     conn = db_connection("choice")
     cursor = conn.cursor()
     cursor.execute(FETCH_ALL_CHOICE)
@@ -63,7 +69,7 @@ def byChoice(genreList, castList):
                 if cast == c:
                     cf += 1
         if cf != 0:
-            sql_query = "Update choice set castFre = " + str(cf)+" where movie_id = "+str(row[1])
+            sql_query = f"Update choice set castFre = {cf} where movie_id = {row[1]}"
             cursor.execute(sql_query)
 
     for row in results:
@@ -74,11 +80,12 @@ def byChoice(genreList, castList):
                 if gen == g:
                     gf += 1
         if gf != 0:
-            sql_query = "Update choice set genFre = "+str(gf)+" where movie_id = "+str(row[1])
+            sql_query = f"Update choice set genFre = {gf} where movie_id = {row[1]}"
             cursor.execute(sql_query)
 
     cursor.execute(SORT_BY_CAST)
     results = cursor.fetchall()
+    conn.close()  # Close the connection
 
     choice_movies = []
     counter = 0
@@ -107,7 +114,7 @@ def byGenre(genre, movies_result):
             mov_gen = list(row[1].split("$"))
             for gen in mov_gen:
                 if gen == genre:
-                    cursor.execute("Select title from popular where movie_id = '"+str(row[0])+"'")
+                    cursor.execute(f"Select title from popular where movie_id = '{row[0]}'")
                     title = cursor.fetchall()
                     genre_movies.append(title[0][0])
                     genre_posters.append(fetchPoster(row[0], movies_result))
@@ -116,6 +123,7 @@ def byGenre(genre, movies_result):
         else:
             break
 
+    conn.close()  # Close the connection
     return genre_movies, genre_posters
 
 
@@ -133,7 +141,7 @@ def byYear(year, movies_result):
     for row in results:
         if counter != 6:
             if year == row[1]:
-                cursor.execute("Select title from popular where movie_id = '"+str(row[0])+"'")
+                cursor.execute(f"Select title from popular where movie_id = '{row[0]}'")
                 title = cursor.fetchall()
                 year_movies.append(title[0][0])
                 year_posters.append(fetchPoster(row[0], movies_result))
@@ -141,4 +149,5 @@ def byYear(year, movies_result):
         else:
             break
 
+    conn.close()  # Close the connection
     return year_movies, year_posters
